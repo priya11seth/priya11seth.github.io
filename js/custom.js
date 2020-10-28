73ff43189e5bad92d390f6b19191be1946a2c966
priyanka <priya11seth@gmail.com>
2020-10-28 17:53:10 +0530

    resolve cart json errors

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,31 @@
 
+async function getData() {
+    let url = 'https://priya11seth.github.io/cart.json';
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function renderUsers() {
+    let users = await getData();
+    let html = '';
+    users.forEach(user => {
+        let htmlSegment = `<div class="user">
+                            <img src="${user.profileURL}" >
+                            <h2>${user.firstName} ${user.lastName}</h2>
+                            <div class="email"><a href="email:${user.email}">${user.email}</a></div>
+                        </div>`;
 
-$(document).ready(function(){
-	var url = "https://github.com/priya11seth/shopping-cart/blob/main/cart.json"
-   fetch(url)
-    .then(response => {
-        // handle the response
-		console.log(response);
-    })
-    .catch(error => {
-        // handle the error
+        html += htmlSegment;
     });
+
+    let container = document.querySelector('.container');
+    container.innerHTML = html;
+}
+$(document).ready(function(){
+	renderUsers();
 });
 
 
