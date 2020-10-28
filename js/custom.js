fe07aad570b334501bf67ee148d9223d553c873d
priyanka <priya11seth@gmail.com>
2020-10-28 19:44:19 +0530

    make resposive

--- js/custom.js
+++ js/custom.js
@@ -13,16 +13,24 @@ async function renderUsers() {
 	console.log(jsonData);
     let html = '';
     jsonData.items.forEach(data => {
-        let htmlSegment = `<div class="user">
-                            <img src="${data.image}" >
-                            <h2>${data.name}</h2>
-                            
-                        </div>`;
+        let htmlSegment = `<li class="cards_item">
+							  <div class="card">
+								<div class="card_image"><img src="${data.image}"></div>
+								<div class="card_content">
+								 <p class="card_text">${data.name}</p>
+								 <div class="btn-div"> 
+									<h2 class="card_title">${data.price.actual}</h2>
+									<button class="btn card_btn">Add to Cart</button>
+								 </div>
+								  
+								</div>
+							  </div>
+							</li>`;
 
         html += htmlSegment;
     });
 
-    let container = document.querySelector('.container');
+    let container = document.querySelector('.cards');
     container.innerHTML = html;
 }
 $(document).ready(function(){
