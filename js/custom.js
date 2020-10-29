9bd8379c8feb62f5fbd610b0bc954edcea7d20d7
priyanka <priya11seth@gmail.com>
2020-10-29 19:16:35 +0530

    final functionality done

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-
+ 
 async function getData() {
     let url = 'https://priya11seth.github.io/cart.json';
     try {
@@ -8,19 +8,23 @@ async function getData() {
         console.log(error);
     }
 }
+
+let jsonData;
 async function renderUsers() {
-    let jsonData = await getData();
+    jsonData = await getData();
 	console.log(jsonData);
     let html = '';
-    jsonData.items.forEach(data => {
+    jsonData.items.forEach((data,index) => {
+		let per = Math.floor((data.price.display-data.price.actual)*100/data.price.display);
         let htmlSegment = `<li class="cards_item">
 							  <div class="card">
+								<div class="discount">${per}% off</div>
 								<div class="card_image"><img src="${data.image}"></div>
 								<div class="card_content">
 								 <p class="card_text">${data.name}</p>
 								 <div class="btn-div"> 
-									<h2 class="card_title"><span class="display-price"><i class="fa fa-inr" aria-hidden="true"></i>${data.price.display}</span> <i class="fa fa-inr" aria-hidden="true"></i>${data.price.actual}</h2>
-									<button class="btn card_btn">Add to Cart</button>
+									<h2 class="card_title"><i class="fa fa-inr" aria-hidden="true"></i>${data.price.actual}<br><span class="display-price"><i class="fa fa-inr" aria-hidden="true"></i>${data.price.display}</span></h2>
+									<button class="btn card_btn" onclick="addItem('${index}')">Add to Cart</button>
 								 </div>
 								  
 								</div>
@@ -36,6 +40,125 @@ async function renderUsers() {
 $(document).ready(function(){
 	renderUsers();
 });
+
+let arr = [];
+function addItem(index){
+	
+	document.getElementById("msgdiv").innerHTML = `<span class="msgSpan">${jsonData.items[index].name} added to cart</span>`;
+	console.log("hello",jsonData.items[index]);
+	jsonData.items[index]['quantity'] = 1;
+	arr.push(jsonData.items[index]);
+	createElementByArr();
+	
+}
+
+function createElementByArr(){
+	
+	let html = '';
+	arr.forEach((data,index) => {
+		
+        let htmlSegment = `<div class="layout-inline row-table">
+        
+						<div class="col col-pro layout-inline">
+						  <img src="${data.image}" alt="kitten" />
+						  <p>${data.name}</p>
+						</div>
+						
+						<div class="col col-price col-numeric align-center ">
+						  <p>${data.price.display}</p>
+						</div>
+
+						<div class="quantity">
+						  <button class="plus-btn" type="button" name="button" onclick="plus('${index}')">
+							
+						  </button>
+						  <input type="text" name="name" value="${data.quantity}">
+						  <button class="minus-btn" type="button" name="button" onclick="minus('${index}')">
+							
+						  </button>
+						</div>
+        			</div>`;
+
+        html += htmlSegment;
+    });
+	
+	var total = 0;
+	var discount = 0;
+	var totalItems = 0;
+	arr.forEach(data=>{
+		total += ( data.price.display * data.quantity );
+		discount += ( data.discount * data.quantity );
+		totalItems += data.quantity ;
+	})
+	
+	let tfSegment = `<div class="tf">
+						<div class="row-table layout-inline">
+						   <div class="col">
+							 <p>Total</p>
+						   </div>
+						   <div class="col"></div>
+						 </div>
+						 <div class="row-table layout-inline">
+						   <div class="col">
+							 <p>Items(${totalItems}):</p>
+						   </div>
+						   <div class="col">${total}</div>
+						 </div>
+						  <div class="row-table layout-inline">
+							   <div class="col">
+								 <p>Discount:</p>
+							   </div>
+								<div class="col">-${discount}</div>
+						</div>		
+						   <div class="row-table layout-inline orderTotal">
+							   <div class="col">
+								 <p>Order Total:</p>
+							   </div>
+								<div class="col">${total - discount}</div>
+						   
+						 
+						 </div></div>`;
+	 	
+		html += tfSegment;		
+						 
+	let container = document.querySelector('.tbl');
+    container.innerHTML = html;
+	
+	
+	
+}
+
+function minus(index) {
+    //e.preventDefault();
+   
+	console.log()
+	if(arr[index].quantity <= 1){
+		arr[index].quantity =  1;
+	}
+	else{
+		arr[index].quantity -=  1;
+	}
+    
+	//arr = [];
+	createElementByArr();
+	
+    
+ 
+ 
+};
+ 
+function plus(index) {
+	
+    arr[index].quantity +=  1;
+	//arr = [];
+	createElementByArr();
+	
+};
+
+
+
+
+   
 
 
 
